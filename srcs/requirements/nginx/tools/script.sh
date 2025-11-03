96ba208cc6db7969588b1f19cdef5302c3a27072
tleister <tleister@student.42heilbronn.de>
2025-11-03 16:20:44 +0000

    changed bonus to an other domain name and used sublocations for different bonuses

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cat /etc/nginx/sites-available/default > /etc/nginx/sites-available/default.bak
-envsubst '$URL' < /etc/nginx/sites-available/default.bak > /etc/nginx/sites-available/default
+envsubst '$URL,$BONUS_URL' < /etc/nginx/sites-available/default.bak > /etc/nginx/sites-available/default
 exec nginx -g 'daemon off;'
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,7 +6,7 @@ if [ ! -f wp-cli.phar ]; then
 	chmod +x wp-cli.phar
 	echo "Setup WordPress..."
 	./wp-cli.phar core download --allow-root
-	./wp-cli.phar config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb --allow-root
+	./wp-cli.phar config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb:3306 --allow-root
 	./wp-cli.phar core install --url="$URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 	echo "Installing redis cache..."
 	./wp-cli.phar plugin install redis-cache --activate --allow-root
